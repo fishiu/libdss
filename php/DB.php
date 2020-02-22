32c4dc341c3756ef2c4352d4e20b88c5aa11301e
LeverestFish <1800016611@pku.edu.cn>
2020-02-22 15:13:09 +0800

    update config

--- php/DB.php
+++ php/DB.php
@@ -7,8 +7,16 @@ class DB {
 	private $password = "yourPassword";
 	private $dbname = "yourDbname";
 
-	public function connection() {
+    public function __construct()
+    {
+        $config = parse_ini_file("../config.ini");
+        $this->servername = $config['servername'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->dbname = $config['dbname'];
+    }
 
+	public function connection() {
 		$conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
 		// 检测连接
 		if ($conn->connect_error) {
--- php/execute.php
+++ php/execute.php
@@ -2,8 +2,18 @@
 error_reporting(0);
 
 class execute {
-	private $PHP_PATH = 'yourPhpPath'; // PHP解释器的路径
-	public function doExecute($url) {
+    private $PHP_PATH = null; // PHP解释器的路径
+
+    /**
+     * execute constructor.
+     */
+    public function __construct()
+    {
+        $config = parse_ini_file("../config.ini");
+        $this->PHP_PATH = $config['phpExecutePath'];
+    }
+
+    public function doExecute($url) {
 		shell_exec($this->PHP_PATH . ' /php/' . $url);
 	}
 }
