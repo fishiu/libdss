1b48c9d39123bff797479831cc3f0bd08e4f919a
LeverestFish <1800016611@pku.edu.cn>
2020-02-22 14:12:54 +0800

    update some config

--- php/execute.php
+++ php/execute.php
@@ -1,13 +1,9 @@
 <?php
 error_reporting(0);
 
-class execute
-{
-    private $PHP_PATH = '/Applications/MAMP/bin/php/php7.4.1/bin/php';
-//    private $PHP_PATH = '/www/server/php/56/bin/php';
-    public function doExecute($url)
-    {
-//        shell_exec('cd php; '.$this->PHP_PATH.' php\'.$url);
-        shell_exec($this->PHP_PATH.' /php/'.$url);
-    }
+class execute {
+	private $PHP_PATH = 'yourPhpPath'; // PHP解释器的路径
+	public function doExecute($url) {
+		shell_exec($this->PHP_PATH . ' /php/' . $url);
+	}
 }
