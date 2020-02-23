a0fd29e1a3786433a6f6ab62f8bdadcc6bfa13c4
LeverestFish <1800016611@pku.edu.cn>
2020-02-23 12:12:23 +0800

    链接映射修改

--- PA_4/pa_4.1.php
+++ PA_4/pa_4.1.php
@@ -49,7 +49,7 @@
             <a href="pa_4.3.php" class="fl">
                 <div class="drop-item"><span class="icon-pie-chart"></span>图书情况</div>
             </a>
-            <a href="pa_4.3.php" class="fl">
+            <a href="pa_4.4.php" class="fl">
                 <div class="drop-item"><span class="icon-pie-chart"></span>用户行为偏好</div>
             </a>
         </div>
